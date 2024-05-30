8d89045a29586ebca3b69d9dd515c7f2334dd1b7
Antonov333 <antonovsergei333@yahoo.com>
2024-05-30 17:01:11 +0300

    Initial commit

---
+++ src/main/java/com/example/text_chat_application/TextChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.text_chat_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextChatApplication.class, args);
+	}
+
+}
